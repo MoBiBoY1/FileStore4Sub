ad2f3ec28b2c9ec63e39107d8c2569d66a5655ca
MoBiBoY1 <115732041+MoBiBoY1@users.noreply.github.com>
2024-05-10 19:59:23 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -89,8 +89,8 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇsғʟɪx', url='https://t.me/Team_Netflix/40')
-                ]
+                    InlineKeyboardButton('🍁 Premium', url='https://t.me/boobx_premium')
+]
             ]
         )
         await message.reply_text(
