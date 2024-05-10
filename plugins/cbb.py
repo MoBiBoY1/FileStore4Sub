c73ed2d4b777311b44e26462e92f1ff406a6fa84
MoBiBoY1 <115732041+MoBiBoY1@users.noreply.github.com>
2024-05-10 19:56:29 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -2,7 +2,6 @@
 
 from pyrogram import __version__
 from bot import Bot
-from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
@@ -10,7 +9,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"Join <b><i>bOObx_Links<i><b>",
+            text = f"<b>Join<i>bOObx_Links<i><b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
